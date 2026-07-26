875b738a6f8a4504715a2823ffa305780563b9f9
Ddfathu <108730817+Ddfathu@users.noreply.github.com>
2026-07-26 09:27:22 +0700

    Change backend domain for request handling

--- functions/[[path]].js
+++ functions/[[path]].js
@@ -5,7 +5,7 @@
 
 export async function onRequest(context) {
   const request = context.request;
-  const BACKEND = "vtes.jibvpn.biz.id"; // Domain Railway Lu
+  const BACKEND = "nodejs-argo-production-7a5d.up.railway.app"; // Domain Railway Lu
 
   // Jika koneksi adalah request WebSocket (Jalur utama VLESS lu)
   if (request.headers.get("Upgrade") === "websocket") {
